35125c09d2a8629fa10426dececfd3ad176330a9
khoi2251120220 <146357980+khoi2251120220@users.noreply.github.com>
2025-03-06 08:31:05 +0700

    bài 2

---
+++ baitaptuan2/baitap2tuan2/baitap2tuan2/Listbox.cs
@@ -0,0 +1,25 @@
+namespace baitap2tuan2
+{
+    public partial class Listbox1 : Form
+    {
+        public Listbox1()
+        {
+            InitializeComponent();
+        }
+
+        private void Listbox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
